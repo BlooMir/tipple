493cc45a5ecb534b21e48a1a8beccd7509157900
bloomir <binarywaterdragon@gmail.com>
2025-05-04 00:15:57 +0900

    page : 메인스크린 스타일 구현

---
+++ client-tipple/src/components/main/SingleLetter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex justify-around">
+    <template v-for="(letter, idx) in line" :key="idx">
+      <q-img
+        v-if="letter !== ','"
+        class="letter"
+        fit="contain"
+        :src="getLetterSrc(letter)"
+        :alt="letter"
+      />
+      <div v-else class="letter domino-box"></div>
+    </template>
+  </div>
+</template>
+
+<script setup>
+// props
+const { line } = defineProps(['line'])
+
+const getLetterSrc = (letter) => {
+  if (letter === ',') return
+  return `/src/assets/letters/${letter}.svg`
+}
+</script>
+
+<style lang="scss" scoped>
+.letter-container {
+  width: 1000px;
+}
+.letter {
+  width: 60px;
+  height: 185px;
+}
+.domino-box {
+  outline: 5px solid $secondary;
+  outline-offset: -5px;
+  border-radius: 3px;
+}
+</style>
--- client-tipple/src/pages/MainScreen.vue
+++ client-tipple/src/pages/MainScreen.vue
@@ -1,9 +1,78 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>메인 스크린</h1>
+  <q-page class="flex justify-center items-center q-pa-md screen-bg">
+    <div class="screen-area">
+      <!-- 첫째 줄 -->
+      <SingleLetter :line="sentence1"></SingleLetter>
+      <div class="domino-floor q-my-lg"></div>
+
+      <!-- 둘째 줄 -->
+      <SingleLetter :line="sentence2"></SingleLetter>
+      <div class="domino-floor q-my-lg"></div>
+
+      <!-- 셋째 줄 -->
+      <SingleLetter :line="sentence3"></SingleLetter>
+      <div class="domino-floor q-my-lg"></div>
+
+      <div class="co2-area column">
+        <span class="co2-amount flex justify-center">12345</span>
+        <span class="co2e flex justify-end">CO&#8322;e</span>
+      </div>
+
+      <!-- 넷째 줄 -->
+      <div class="domino-floor q-my-lg"></div>
+      <SingleLetter :line="sentence4"></SingleLetter>
+
+      <!-- 다섯째 줄 -->
+      <div class="domino-floor q-my-lg"></div>
+      <SingleLetter :line="sentence5"></SingleLetter>
+
+      <!-- 여섯째 줄 -->
+      <div class="domino-floor q-my-lg"></div>
+      <SingleLetter :line="sentence6"></SingleLetter>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-//
+import SingleLetter from '../components/main/SingleLetter.vue'
+
+const sentence1 = 'one,small'.split('')
+const sentence2 = 'acts,can,'.split('')
+const sentence3 = 'cause,big'.split('')
+const sentence4 = 'resulthat'.split('')
+const sentence5 = 'is,domino'.split('')
+const sentence6 = 'effect,,,'.split('')
 </script>
+
+<style lang="scss" scoped>
+.screen-bg {
+  background-color: #000;
+  color: white;
+  font-size: 40px;
+}
+.screen-area {
+  width: 1000px;
+}
+.domino-floor {
+  width: 1000px;
+  height: 18px;
+  border: 3px solid $secondary;
+  border-radius: 3px;
+}
+.co2-area {
+  height: 310px;
+  color: $secondary;
+  font-family: 'Sarpanch', sans-serif;
+  .co2-amount {
+    height: 220px;
+    font-weight: 700;
+    font-size: 200px;
+    letter-spacing: 20px;
+  }
+  .co2e {
+    font-weight: 400;
+    font-size: 50px;
+    padding-right: 40px;
+  }
+}
+</style>
