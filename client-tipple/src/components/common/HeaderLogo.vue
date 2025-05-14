d798611c82f8cb6d72cae656976b1874a96e8c36
bloomir <binarywaterdragon@gmail.com>
2025-05-15 00:02:39 +0900

    feat : 메일 페이지 제작 중

---
+++ client-tipple/src/components/common/HeaderLogo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="logo-container" style="gap: 30px;">
+    <!-- 왼쪽 도미노 -->
+    <div class="wing">
+      <img v-if="leftwing" src="../../assets/left-domino.svg" class="left-wing"></img>
+    </div>
+
+    <span class="fs44 fw600 text-white" >{{ pagename }}</span>
+
+    <!-- 오른쪽 도미노 -->
+    <div class="wing">
+      <img v-if="rightwing" src="../../assets/right-domino.svg"  class="right-wing"></img>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// props
+const { pagename, leftwing, rightwing } = defineProps(['pagename', 'leftwing', 'rightwing'])
+</script>
+
+<style lang="scss" scoped>
+.logo-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 560px;
+  .wing {
+    min-width: 100px;
+    max-width: 200px;
+    min-height: 16px;
+    max-height: 32px;
+    .left-wing {
+      width: 100%;
+      height: 100%;
+    }
+    .right-wing {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
--- client-tipple/src/pages/MailDesktop.vue
+++ client-tipple/src/pages/MailDesktop.vue
@@ -1,9 +1,28 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>메일 데스크톱</h1>
+  <q-page class="tipple-primary-to-bottom">
+    <header class="mail-header flex items-center q-pl-xl col-3">
+      <HeaderLogo pagename="Mail" :leftwing="true" :rightwing="true"></HeaderLogo>
+    </header>
+
+    <article class="flex col-9">
+      <div class="mail-article-left fs32" style="flex: 0 0 25%">123</div>
+      <div class="mail-article-right fs32" style="flex: 0 0 75%">123</div>
+    </article>
   </q-page>
 </template>
 
 <script setup>
-//
+import HeaderLogo from 'src/components/common/HeaderLogo.vue'
 </script>
+
+<style lang="scss" scoped>
+.mail-header {
+  height: 190px;
+}
+.mail-article-left {
+  background-color: rgba(255, 255, 255, 0.32);
+}
+.mail-article-right {
+  background-color: rgba(0, 0, 0, 0.46);
+}
+</style>
